520942765d20e71eada242b2efb5a50a2bcfa9a5
BlackHawkPL <arturo2028@gmail.com>
2018-03-07 13:33:19 +0100

    removed sounds

--- cfgSounds.hpp
+++ cfgSounds.hpp
@@ -4,13 +4,13 @@ class CfgSounds
 	class ITC_AP_enabled
 	{
 		name = "Autopilot enabled";
-		sound[] = {"itc_air\data\Sounds\ap_enabled.ogg", db+10, 1.0};
+		sound[] = {"", db+10, 1.0};
 		titles[] = {0,""};
 	};
 	class ITC_AP_warning
 	{
 		name = "Autopilot warning";
-		sound[] = {"itc_air\data\Sounds\ap_warning.ogg", db+10, 1.0};
+		sound[] = {"", db+10, 1.0};
 		titles[] = {0,""};
 	};
 };
